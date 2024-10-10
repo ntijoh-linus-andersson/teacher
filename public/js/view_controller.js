09a7ead7799660cfdf6b0a2373bfff1387423576
Linus0812 <linus.andersson.0812@outlook.com>
2024-10-10 10:22:17 +0200

    f

--- public/js/view_controller.js
+++ public/js/view_controller.js
@@ -42,37 +42,15 @@ class ViewController extends HTMLElement {
 
     async #buildRepos(ownerName) {
         this.#resetView();
-    
-        try {
-            // Try to fetch repo data from the API
-            const data = await this.#getRepo(ownerName);
-    
-            // Check if no data is returned (e.g., user not found)
-            if (!data || data.length === 0) {
-                const template = document.createElement('template');
-                template.innerHTML = `
-                    <h1>User not found or has no repositories</h1>
-                <img src="/img/no-matches.jpeg" style="width: 50%; height: auto;">
+        const data = await this.#getRepo(ownerName);
 
-                `;
-                this.shadowRoot.appendChild(template.content.cloneNode(true));
-                return;
-            }
-    
-            // If data exists, iterate over repos and create RepoCard elements
-            data.forEach(repo => {
-                this.shadowRoot.appendChild(new RepoCard(repo));
-            });
-    
-        } catch (error) {
-            // If there was an error during the API call (e.g., user not found or network error)
-            const template = document.createElement('template');
-            template.innerHTML = `
-                <h1>Error: ${error.message}</h1>
-            `;
-            this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+        if (!data) {
+            return;
         }
+            this.shadowRoot.appendChild(new RepoContainer(data))
     }
+
     
     async #buildForks(ownerName, repoName) {
         this.#resetView();
